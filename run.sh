f83d1840678629ecabb374a55a756d0978855ce3
Vu Viet Tai <vuviettai@gmail.com>
2024-03-21 14:38:55 +0700

    Update run script

--- run.sh
+++ run.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-RUST_LOG=info ${SCRIPT_DIR}/ord -t --bitcoin-rpc-password bitcoincodelight \
-    --bitcoin-rpc-url http://222.253.82.244:18332 \
+
+testnet() {
+    RUST_LOG=info ${SCRIPT_DIR}/ord -t --bitcoin-rpc-password bitcoincodelight \
+    --bitcoin-rpc-url http://192.168.1.254:18332 \
     --bitcoin-rpc-username bitcointestnet \
     --index-runes --index-transactions \
     --index ./index.redb \
     server --address 0.0.0.0 --http-port 8088 > ord.log 2>&1
+}
+
+debug() {
+    RUST_LOG=debug cargo run -- -t --bitcoin-rpc-password bitcoincodelight \
+    --bitcoin-rpc-url http://192.168.1.254:18332 \
+    --bitcoin-rpc-username bitcointestnet \
+    --index-runes --index-transactions \
+    --index /Users/viettai/workspace/bitcoin/runebeta_ord/index.redb \
+    server --address 0.0.0.0 --http-port 8088
+}
+
+$@
