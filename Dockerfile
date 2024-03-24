8ba447594126503cae93130b8414ddb5c550180d
Vu Viet Tai <vuviettai@gmail.com>
2024-03-24 09:01:47 +0700

    Use cargo 1.77.0

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM rust:1.75.0-bookworm as builder
+FROM rust:1.77.0-bookworm as builder
 
 WORKDIR /usr/src/ord
 
@@ -9,7 +9,7 @@ RUN cargo build --bin ord --release
 FROM debian:bookworm-slim
 
 COPY --from=builder /usr/src/ord/target/release/ord /usr/local/bin
-RUN apt-get update && apt-get install -y openssl
+RUN apt-get update && apt-get install -y openssl libpq-dev
 
 ENV RUST_BACKTRACE=1
 ENV RUST_LOG=info
--- run.sh
+++ run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-testnet() {
-    RUST_LOG=info ${SCRIPT_DIR}/ord -t --bitcoin-rpc-password bitcoincodelight \
-    --bitcoin-rpc-url http://192.168.1.254:18332 \
-    --bitcoin-rpc-username bitcointestnet \
+BITCOIN_RPC_URL_TESTNET=http://192.168.1.253:18332
+BITCOIN_RPC_URL_MAINNET=http://192.168.1.253:8332
+main() {
+    RUST_LOG=info ${SCRIPT_DIR}/ord \
+    --bitcoin-rpc-url http://192.168.1.253:8332 \
     --index-runes --index-transactions \
-    --index ./index.redb \
-    server --address 0.0.0.0 --http-port 8088 > ord.log 2>&1
+    --index /mnt/ordinals/codelight/bitcoin/ord/data/0.16.0/runebeta_index.redb \
+    server --address 0.0.0.0 --http-port 8090 > runebeta_ord.log 2>&1
+}
+#For docker
+testnet() {
+    RUST_LOG=info /usr/local/bin/ord -t \
+    --bitcoin-rpc-url ${ORD_BITCOIN_RPC_URL} \
+    --index-runes --index-transactions \
+    --index /opt/data/runebeta_index.redb \
+    server --address 0.0.0.0 --http-port 8088 > /opt/ord.log 2>&1
 }
 
 debug() {
@@ -19,4 +27,7 @@ debug() {
     server --address 0.0.0.0 --http-port 8088
 }
 
+tunnel() {
+    ssh -L 8090:192.168.1.253:8090 scalar
+}
 $@
